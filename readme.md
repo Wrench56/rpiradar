c3b5833dfa4e90e8a356b2dd8e134bd904fc19ee
Wrench56 <markdevenyidev@gmail.com>
2025-01-28 12:10:33 -0500

    feat: initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# RPiRadar
+
+A tool made for discovering the local IP address of Raspberry Pi-s
+
---
+++ src/main.py
@@ -0,0 +1,10 @@
+'''Main file'''
+
+
+def main() -> None:
+    '''Entry point'''
+    print('Hello World')
+
+
+if __name__ == '__main__':
+    main()
